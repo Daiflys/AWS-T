62d1840ac1be8166c8f2561d2d09428e7d80126f
Daiflys s <daiflys@gmail.com>
2025-05-22 16:19:38 +0000

    etag examples with terraform (and leftovers from yesterday session, maybe we can get rid of almost everything there)

--- s3/etags/main.tf
+++ s3/etags/main.tf
@@ -12,18 +12,17 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
-resource "aws_s3_bucket" "bucket" {
-  Name = "My bucket"
-  Environment = "Dev"
+resource "aws_s3_bucket" "default" {
 }
 
 resource "aws_s3_object" "object" {
-  bucket = resource aws_s3_bucket default
+  bucket = aws_s3_bucket.default.id
   key    = "myfile.txt"
   source = "myfile.txt"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("path/to/file")
+  
+  etag = filemd5("myfile.txt")
 }
